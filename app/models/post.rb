ac309e5f779beb71c612168f41801194cffeb7f3
Lily Parmenter <parmenter.lily@gmail.com>
2015-06-27 16:24:47 -0500

    Assignment 38 completed, Associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   default_scope { order('created_at DESC')}
+  scope :ordered_by_title, -> { reorder(title: :asc) }
+  scope :ordered_by_reverse_created_at, -> { reorder(created_at: :asc) }
 end
