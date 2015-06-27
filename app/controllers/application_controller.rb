bdc07dba74614f5101518daf652a7e71d2f0cfbe
Lily Parmenter <parmenter.lily@gmail.com>
2015-06-26 21:02:47 -0500

    Assignment 37 completed, Authentication Continued

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :flash_attack
+  
+  def flash_attack
+    flash[:notice] = "Test flash"
+  end
   protect_from_forgery with: :exception
 end
