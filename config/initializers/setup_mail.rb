2a2274fcc20ec0936b331ccb90e2d410862c0f31
Lily Parmenter <parmenter.lily@gmail.com>
2015-06-27 13:36:50 -0500

    Checkpoint 37 updated, Authentication Continued

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,8 +4,8 @@ if Rails.env.development?
     address:        'smtp.sendgrid.net',
     port:           '587',
     authentication: :plain,
-    user_name:      ENV['app37230053@heroku.com'],
-    password:       ENV['m4uobdn84867'],
+    user_name:      ENV['SENDGRID_USERNAME'],
+    password:       ENV['SENDGRID_PASSWORD'],
     domain:         'heroku.com',
     enable_starttls_auto: true
   }
