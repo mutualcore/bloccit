1d7195e1eb737e4f24c158b0b15363ce66ade04d
Lily Parmenter <parmenter.lily@gmail.com>
2015-06-26 19:15:15 -0500

    NOT DONE WITH ASSIGNMENT 35

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,39 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions= Question.all
+  end
+
+  def show
+    @questions = Question.find(params[:id])
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(params.require(:question).permit(:title, :body))
+    if @question.save
+      flash[:notice] = "Question was saved."
+      redirect_to @question
+    else
+      flash[:error] = "There was an error saving the question. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update_attributes(params.require(:question).permit(:title, :body, :resolved))
+      flash[:notice] = "Question was updated."
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the question. Please try again."
+      render :edit
+    end
+  end
+end
