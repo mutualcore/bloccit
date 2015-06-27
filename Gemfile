987bcd127662572fb98ac4cd08dc41b5c4917058
Lily Parmenter <parmenter.lily@gmail.com>
2015-06-27 17:41:16 -0500

    Checkpoint 39 completed, Authorization

--- Gemfile
+++ Gemfile
@@ -44,3 +44,4 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'faker'
 gem 'devise'
 gem 'pry-rails'
+gem 'pundit'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150627190159) do
+ActiveRecord::Schema.define(version: 20150627213236) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20150627190159) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
