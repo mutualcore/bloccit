17bf030f5cf6991d1feecee99386472c8dab429c
Lily Parmenter <parmenter.lily@gmail.com>
2015-06-29 20:38:51 -0500

    work in progress

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,4 +1,25 @@
 class PostPolicy < ApplicationPolicy
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.admin? || user.moderator?
+        scope.all
+      else
+        scope.where(:published => true)
+      end
+    end
+  end
+
+  def update?
+    user.admin? or not post.published?
+  end
+
   def index?
     true
   end
