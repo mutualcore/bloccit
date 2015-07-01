2518105242daf274ade9c937945bd7c72ba57be7
Lily Parmenter <parmenter.lily@gmail.com>
2015-06-30 19:25:20 -0500

    Work in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,10 +20,19 @@ require 'faker'
  
  # The `save` method then saves this User to the database.
 
+# Create Topics
+15.times do
+  Topic.create!(
+    name:   Faker::Lorem.sentence,
+    description:  Faker::Lorem.paragraph)
+end
+topics = Topic.all
+
 # Create Posts
 50.times do
   Post.create!(
     user: users.sample,
+    topic: topics.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
     )
@@ -39,12 +48,23 @@ posts = Post.all
     )
 end
 
-user = User.first
-user.skip_reconfirmation!
-user.update_attributes!(
+admin = User.new(
+  name: 'Admin User', 
   email: 'parmenter.lily@gmail.com',
-  password: 'lilzpeach')
+  password: 'Password123',
+  role: 'admin')
+admin.skip_confirmation!
+admin.save!
+
+moderator = User.new(
+  name: 'Moderator User',
+  email: 'moderator@example.com',
+  password: 'helloworld',
+  role: 'moderator')
+moderator.skip_confirmation!
+moderator.save!
 
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
