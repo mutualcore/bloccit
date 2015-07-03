76dcbda3bcc10bafaaa5cf63848db863834eeb27
Lily Parmenter <parmenter.lily@gmail.com>
2015-07-03 16:50:25 -0500

    Interlude

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
 
   def create
      @topic = Topic.find(params[:topic_id])
-     @post = Post.new(params.require(:post).permit(:title, :body))
+     @post = Post.new(post_params)
      @post.user = current_user
      @post.topic = @topic
      authorize @post
@@ -38,12 +38,18 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     authorize @post
 
-    if @post.update_attributes(params.require(:post).permit(:title, :body))
+    if @post.update_attributes(post_params)
       flash[:notice] = "Post was updated."
       redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :edit
     end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
