b131b890823fb14458ba0ba795709cea18650c4d
Lily Parmenter <parmenter.lily@gmail.com>
2015-07-03 18:11:18 -0500

    Checkpoint 42 completed, Validating Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,9 @@ class Post < ActiveRecord::Base
   default_scope { order('created_at DESC')}
   scope :ordered_by_title, -> { reorder(title: :asc) }
   scope :ordered_by_reverse_created_at, -> { reorder(created_at: :asc) }
+
+  validates :title, length: {minimum: 5}, presence: true
+  validates :body, length: {minimum: 20}, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 end
