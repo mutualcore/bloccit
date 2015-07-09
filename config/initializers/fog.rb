491efd8114ec6c03e78a058fb2d671278ffa0aaa
Lily Parmenter <parmenter.lily@gmail.com>
2015-07-08 20:26:35 -0500

    Checkpoint 44, work in progress

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:               'AWS',
-    aws_access_key_id:      'AKIAJSPUF6AGBILOZM6Q',
-    aws_secret_access_key:  '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'
-  }
-  config.fog_directory  = ENV['AWS_BUCKET']
+      :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+    config.fog_directory     =  ENV['AWS_BUCKET']
   config.fog_public     = true
 end
