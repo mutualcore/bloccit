0045834a0126254d8c4623cb71b3fea2c7bb3528
Lily Parmenter <parmenter.lily@gmail.com>
2015-07-13 18:22:53 -0500

    Work in progress

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,17 @@
 class CommentsController < ApplicationController
   def create
-     @user = current_user
      @post = Post.find(params[:post_id])
-     @comment = @post.comments.create(params[:body])
-     @comment.save
+     @comment = current_user.comments.build(comment_params)
+     @comment.post_id = @post.id
+     if @comment.save
+      flash[:notice] = "Comment was saved."
+     else
+      flash[:error] = "There was an error saving the comment. Please try again."
+     end
      redirect_to [@post.topic, @post]
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 end
