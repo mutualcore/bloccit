9f3c5dc30aab279098e8cea72620434844f5eff3
Lily Parmenter <parmenter.lily@gmail.com>
2015-07-15 20:29:58 -0500

    Assignment 46 almost completed, Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
      @post = Post.find(params[:post_id])
      @comment = current_user.comments.build(comment_params)
      @comment.post_id = @post.id
+     authorize @comment
      
      if @comment.save
       flash[:notice] = "Comment was saved."
@@ -10,6 +11,10 @@ class CommentsController < ApplicationController
       flash[:error] = "There was an error saving the comment. Please try again."
      end
      redirect_to [@post.topic, @post]
+  end
+
+  def new
+    authorize @comment
   end
 
   def comment_params
