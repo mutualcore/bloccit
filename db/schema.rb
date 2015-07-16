141f4fe651c2a6aaa928232eeddb92ed96e7a9f6
Lily Parmenter <parmenter.lily@gmail.com>
2015-07-15 20:06:19 -0500

    Fixed comments

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 20150709232644) do
+ActiveRecord::Schema.define(version: 20150714010535) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
