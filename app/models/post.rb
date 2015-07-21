259d89314cc3754e4922158ba3ba0428ef984164
Lily Parmenter <parmenter.lily@gmail.com>
2015-07-21 14:30:01 -0500

    Assignment 49 completed, Voting

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,17 @@ class Post < ActiveRecord::Base
   belongs_to :topic
   mount_uploader :image, ImageUploader
 
+  default_scope { order('rank DESC')}
+  scope :ordered_by_title, -> { reorder(title: :asc) }
+  scope :ordered_by_reverse_created_at, -> { reorder(created_at: :asc) }
+
+  validates :title, length: {minimum: 5}, presence: true
+  validates :body, length: {minimum: 20}, presence: true
+  # validates :topic, presence: true
+  # validates :user, presence: true
+
+  after_create :create_vote
+
   def up_votes
     votes.where(value: 1).count
   end
@@ -17,19 +28,18 @@ class Post < ActiveRecord::Base
     votes.sum(:value)
   end
 
-  default_scope { order('rank DESC')}
-  scope :ordered_by_title, -> { reorder(title: :asc) }
-  scope :ordered_by_reverse_created_at, -> { reorder(created_at: :asc) }
+  def update_rank
+    age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
+    new_rank = points + age_in_days
 
-  validates :title, length: {minimum: 5}, presence: true
-  validates :body, length: {minimum: 20}, presence: true
-  # validates :topic, presence: true
-  # validates :user, presence: true
-   def update_rank
-     age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
-     new_rank = points + age_in_days
- 
-     update_attribute(:rank, new_rank)
-   end
+    update_attribute(:rank, new_rank)
+  end
+
+  private
+
+  def create_vote
+    @post = self
+    user.votes.create(value: 1, post: @post)
+  end
 
 end
