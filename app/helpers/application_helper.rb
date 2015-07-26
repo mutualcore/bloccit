1b38edd4e1698ef88e03ec93067a690ba0754f34
Lily Parmenter <parmenter.lily@gmail.com>
2015-07-26 15:19:21 -0500

    Assignment 53 completed, Another Interlude

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,5 +12,13 @@ module ApplicationHelper
     extensions = {fenced_code_blocks: true}
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render markdown).html_safe
-  end
+   end
+
+   def up_vote_link_classes(post)
+    return "glyphicon glyphicon-chevron-up #{(current_user.voted(post) && current_user.voted(post).up_vote?) ? 'voted' : '' }"
+   end
+
+   def down_vote_link_classes(post)
+    return "glyphicon glyphicon-chevron-down #{(current_user.voted(post) && current_user.voted(post).down_vote?) ? 'voted' : '' }"
+   end
 end
