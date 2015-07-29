8c6baba5ebf97648b68a0961a44d96bc84307edf
Lily Parmenter <parmenter.lily@gmail.com>
2015-07-29 14:14:51 -0500

    Assignment 56 completed, Testing Users

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -6,4 +6,11 @@ FactoryGirl.define do
     password_confirmation "helloworld"
     confirmed_at Time.now
   end
+
+  factory :user_with_post_and_comment do
+    after :build do | user |
+     post = create(:post, user: user)
+     create(:comment, user: user, post: post)
+    end
+  end
 end
